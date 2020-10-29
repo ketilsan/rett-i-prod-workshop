f9b3c33527f67b33d1da5eee1265fafec9580a15
Ketil Velle <ketil.velle@bekk.no>
2020-10-29 14:22:34 +0100

    remove failing test

--- src/controller/super-important-service.test.js
+++ src/controller/super-important-service.test.js
@@ -4,7 +4,3 @@ test('hello should be world', () => {
     const { hello } = superImportantService();
     expect(hello).toBe("world");
 });
-
-test('This should fail', () => {
-    expect(superImportantService()).toBe("world!");
-});
